ea73bef4b42065c5ec5fd13220e1494c8fdfd104
Anshul Bhilare <terminatoranshul@gmail.com>
2026-08-24 23:11:03 +0530

    Update AccountService.java

--- src/main/java/com/webapp/bankingportal/service/AccountService.java
+++ src/main/java/com/webapp/bankingportal/service/AccountService.java
@@ -1,7 +1,7 @@
 package com.webapp.bankingportal.service;
 import com.webapp.bankingportal.entity.Account;
 import com.webapp.bankingportal.entity.User;
-public interface AccountService {
+public interface AccountService {{
 	public Account createAccount(User user);
 	public boolean isPinCreated(String accountNumber) ;
 	public void createPin(String accountNumber, String password, String pin) ;
